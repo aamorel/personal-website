53e225602874861a75cb8678394ed6f4fe4a3455
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2024-03-09 11:28:47 +0100

    first chess commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Chess } from "chess.js";
+import "./App.css";
+import CustomChessboard from "./components/Chessboard";
+
+const problems: string[] = ["1k6/8/1K6/8/8/8/8/7Q w - - 0 1"];
+
+enum NavigationState {
+  Problem,
+  Home,
+}
 
 function App() {
+  // States
+  const [navigationState, setNavigationState] = useState<NavigationState>(
+    NavigationState.Problem
+  );
+  const [firstProblemKey, setFirstProblemKey] = useState<number>(0);
+  const [problemIndex, setProblemIndex] = useState<number>(0);
+
+  const afterFirstMove = (game: Chess) => {
+    // White should have won
+    if (game.isCheckmate()) {
+      setNavigationState(NavigationState.Home);
+    } else {
+      setFirstProblemKey((prev) => prev + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="h-screen w-screen overflow-hidden flex flex-col items-center justify-center">
+      {navigationState === NavigationState.Problem && (
+        <div className="w-1/2 flex flex-col items-center">
+          <p>Mate me in one move to enter</p>
+          <CustomChessboard
+            initialPosition={problems[problemIndex]}
+            afterFirstMove={afterFirstMove}
+            key={firstProblemKey}
+          />
+        </div>
+      )}
+      {navigationState === NavigationState.Home && (
+        <div className="w-1/2">
+          <h1 className="text-3xl font-bold">You solved the first problem!</h1>
+        </div>
+      )}
+    </main>
   );
 }
 
---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,60 @@
+import { useState, useRef } from "react";
+import { Square, Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+
+interface CustomChessboardProps {
+  initialPosition?: string;
+  afterFirstMove: (game: Chess) => void;
+}
+
+export default function CustomChessboard({
+  initialPosition,
+  afterFirstMove,
+}: CustomChessboardProps) {
+  const game = useRef(
+    initialPosition ? new Chess(initialPosition) : new Chess()
+  );
+  const [gamePosition, setGamePosition] = useState<string>(game.current.fen());
+
+  function makeAMove(
+    move:
+      | string
+      | {
+          from: Square;
+          to: Square;
+          promotion?: "q" | "r" | "b" | "n";
+        }
+  ) {
+    const result = game.current.move(move);
+    setGamePosition(game.current.fen());
+    return result; // null if the move was illegal, the move object if the move was legal
+  }
+
+  function makeRandomMove() {
+    const possibleMoves = game.current.moves();
+    if (
+      game.current.isGameOver() ||
+      game.current.isDraw() ||
+      possibleMoves.length === 0
+    )
+      return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    makeAMove(possibleMoves[randomIndex]);
+  }
+
+  function onDrop(sourceSquare: Square, targetSquare: Square) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+    // illegal move
+    if (move === null) return false;
+    setTimeout(() => {
+      afterFirstMove(game.current);
+    }, 300);
+    return true;
+  }
+
+  return <Chessboard position={gamePosition} onPieceDrop={onDrop} />;
+}
